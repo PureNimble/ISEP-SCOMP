6165939f2181d13b94c81396ed5694f8d1929313
vscosousa <vscosousa@gmail.com>
2023-03-16 14:47:04 +0000

    Exercícios

--- PL1/ex06/ex06d/main.c
+++ PL1/ex06/ex06d/main.c
@@ -7,19 +7,24 @@
 int main() {
     int i, status;
     pid_t listPids[4];
+
     for (i = 0; i < 4; i++){
         if ((listPids[i] = fork()) == 0) {
             printf("Processo %d com PID '%d' terminou com sucesso.\n", i+1, getpid());
             exit(i+1);
         }
+    }
+
+    for (i = 0; i < 4; i++) {
         if(listPids[i] % 2 == 0){
-            printf("PID par(%d) em execução...\n", listPids[i]);
+            printf("PID par %d está em execução...\n", listPids[i]);
             waitpid(listPids[i], &status, 0);
             if(WIFEXITED(status)){
-                printf("Pid '%d'com retorno de %d.\n",listPids[i], WEXITSTATUS(status));
+                printf("PID par '%d' terminou com status %d.\n",listPids[i], WEXITSTATUS(status));
             }
         }
     }
-    printf ("This is the end.\n");
+
+    printf("This is the end.\n");
     return 0;
 }
--- PL1b/ex03/main.c
+++ PL1b/ex03/main.c
@@ -6,25 +6,8 @@
 #include <sys/wait.h>
 
 int main(void){
-  pid_t pid, pidList[3];
-  int i;
-  char signal[];
-  for(i = 0; i < 3; i++){
-    if((pidList[i] = fork()) == 0){
       for( ; ; ){
-        printf("\nEu sou o %d filho", i+1);
-        sleep(4);
+        printf("I love SCOMP !\n");
+        sleep(1);
       }
-    }
-  }
-  printf("\nA que processo deseja enviar o sinal?");
-  for(i = 0; i < 3; i++){
-    printf("\nPid:%d", pidList[i]);
-  }
-  printf("\nPid:");
-  scanf("%i", &pid);
-  printf("\nSinal:");
-  scanf("%s", &signal);
-  kill(pid, signal);
-  return 0;
 }

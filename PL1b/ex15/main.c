f1cd3bc61b09d4112ab54547abebf71d9e5192a7
Rafael Araújo <1201804@isep.ipp.pt>
2023-03-29 11:48:32 +0100

    Exercícios

---
+++ PL1b/ex15/main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct{
+    char cmd[32]; // Command name.
+    int time_cap; // Time limit to complete (in seconds).
+} command_t;
+
+command_t *cur_cmd = NULL;
+
+void handler(int signo, siginfo_t *sinfo, void *context){
+    char msg [85];
+    sprintf(msg, "The command %s did not complete in its allowed time!\n", cur_cmd->cmd);
+    write(STDOUT_FILENO, msg, strlen(msg));
+}
+
+int main(void){
+    int i;
+    struct sigaction act;
+    memset(&act, 0, sizeof(struct sigaction));
+    sigemptyset(&act.sa_mask);
+    act.sa_sigaction = handler;
+    sigaction(SIGALRM, &act, NULL);
+
+    command_t cmds[5];
+    strcpy(cmds[0].cmd, "cat filename1 filename2>file2&3");
+    cmds[0].time_cap = 5;
+    strcpy(cmds[1].cmd, "ls -R");
+    cmds[1].time_cap = 13;
+    strcpy(cmds[2].cmd, "mv file.txt /home");
+    cmds[2].time_cap = 12;
+    strcpy(cmds[3].cmd, "cp scenery.jpg /home");
+    cmds[3].time_cap = 8;
+    strcpy(cmds[4].cmd, "rm -r");
+    cmds[4].time_cap = 11;
+    
+    for(i = 0; i < 5; i++){
+        cur_cmd = &cmds[i];
+        printf("Comando %s em execução\n", cur_cmd->cmd);
+        alarm(cur_cmd->time_cap);
+        sleep(10);
+    }
+    
+    return 0;
+}
--- PL1b/ex16/main.c
+++ PL1b/ex16/main.c
@@ -9,77 +9,68 @@
 
 #define NUMBER_OF_CHILDREN 50
 
-volatile sig_atomic_t counter_handlerSIGUSR1 = 0;
+volatile sig_atomic_t counter_SIGUSR1 = 0;
 volatile sig_atomic_t counter_handler = 0;
 
-
-void handler(int signo, siginfo_t *sinfo, void *context){
-
+void handleFather(int signo, siginfo_t *sinfo, void *context){
     if(signo == SIGUSR1){
-        counter_handlerSIGUSR1++;
+        counter_SIGUSR1++;
     }
     counter_handler++;
+}
 
+void handleChild(int signo, siginfo_t *sinfo, void *context){
+    simulate2();
+    exit(0);
 }
 
 int simulate1(){
-
-    int n;
-    int result;
+    int n, result;
     time_t t;
-    printf("estou aqui, no simulate1");
     /* intializes RNG (srand():stdlib.h; time(): time.h) */
     srand ((unsigned) time(&t));
 
     /* initialize n */
     n = rand() % 10 + 1;
-    result = rand() % 500;
+    result = rand() % 100;
 
-    if (result > 50){
+    if (result > 35){
         result = 0;
     }
     else result = 1;
 
-
     sleep(n);
     return result;
-
 }
-
 
 int simulate2(){
-
-    int n;
-    int result;
+    int n, result;
     time_t t;
     /* intializes RNG (srand():stdlib.h; time(): time.h) */
     srand ((unsigned) time(&t));
 
     /* initialize n */
     n = rand() % 10 + 1;
-    result = rand() % 500;
+    result = rand() % 1;
 
-    if (result > 50){
-        result = 0;
-    }
-    else result = 1;
-
-    printf("sou eu, o simulate2");
     sleep(n);
     return result;
-
 }
 
+void childSignalSender(pid_t pidList[], int signal){
+    int i;
+    for(i = 0; i < NUMBER_OF_CHILDREN; i++){
+        kill(pidList[i], signal);
+    }
+}
 
 int main(void){
-    int i, valor;
+    int i;
     pid_t pidList[NUMBER_OF_CHILDREN], parentPid = getpid();
+
     struct sigaction act;
     memset(&act, 0, sizeof(struct sigaction));
     sigemptyset(&act.sa_mask);
-    act.sa_sigaction = handler;
-    sigaction(SIGUSR1, &act, NULL);
-    sigaction(SIGUSR2, &act, NULL);
 
     for(i = 0; i < NUMBER_OF_CHILDREN; i++){
         pidList[i] = fork();
@@ -88,30 +79,28 @@ int main(void){
             exit(-1);
         }
         if(pidList[i] == 0){
-            printf("estou aqui, nos filhos");
-            valor = simulate1();
-            if(valor == 1){
+            act.sa_sigaction = handleChild;
+            sigaction(SIGUSR1, &act, NULL);
+
+            if(simulate1() == 1){
                 kill(parentPid, SIGUSR1);
             }
             else kill(parentPid, SIGUSR2);
             pause();
-            act.sa_handler = SIG_IGN;
-            sigaction(SIGUSR1, &act, NULL);
-            simulate2();
-            exit(0);
+            exit(-1);
         }
-        while(counter_handler < 25);
-        if(counter_handlerSIGUSR1 == 0){
-            printf("Inefficient algorithm!");
-            for(i = 0; i < NUMBER_OF_CHILDREN; i++){
-                kill(pidList[i], SIGKILL);
-            }
-        }
-        else{
-            for(i = 0; i < NUMBER_OF_CHILDREN; i++){
-                kill(pidList[i], SIGUSR1);
-            }
-        }
+    }
+    act.sa_sigaction = handleFather;
+    sigaction(SIGUSR1, &act, NULL);
+    sigaction(SIGUSR2, &act, NULL);
+    while(counter_handler < 25);
+    if(counter_SIGUSR1 == 0){
+        printf("Inefficient algorithm!\n");
+        childSignalSender(pidList, SIGKILL);
+    }else{
+        childSignalSender(pidList, SIGUSR1);
+        while(wait(NULL) > 0);
+        printf("Sucesso!\n");
     }
     return 0;
 }

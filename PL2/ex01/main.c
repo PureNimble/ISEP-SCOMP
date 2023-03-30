8dac56827b2708e4b5b8ba8495b74398e1cd98dd
Rafael Araújo <1201804@isep.ipp.pt>
2023-03-30 14:25:41 +0100

    Exercícios

---
+++ PL2/ex01/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define BUFFER_SIZE 80
+
+int main(void){
+    pid_t pid;
+    int fd[2];
+    if(pipe(fd) == -1){
+        perror("Erro no Pipe");
+        return 1;
+    }
+    pid = fork();
+    if(pid < 0){
+      perror("Erro ao criar o processo");
+      return -1;
+    }
+    if(pid == 0){
+        char read_msg[BUFFER_SIZE];
+        close(fd[1]);
+        read(fd[0], read_msg, BUFFER_SIZE);
+        printf("Filho leu: %s", read_msg);
+        close(fd[0]);
+        exit(0);
+    }
+    pid_t parentPid = getpid();
+    char write_msg[BUFFER_SIZE];
+    sprintf(write_msg, "Se me estiveres a ouvir o meu pid é: %d\n", parentPid);
+    printf("PID: %d\n", parentPid);
+    close(fd[0]);
+    write(fd[1],write_msg,strlen(write_msg)+1);
+    close(fd[1]);
+    return 0;
+}

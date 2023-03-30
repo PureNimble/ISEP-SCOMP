05344fba4f06bc9cbb45e271ec8991ca06038995
vscosousa <vscosousa@gmail.com>
2023-03-30 14:53:00 +0100

    Exercícios

--- PL2/ex03/main.c
+++ PL2/ex03/main.c
@@ -3,12 +3,14 @@
 #include <sys/types.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/wait.h>
 
-#define BUFFER_SIZE 50
+#define BUFFER_SIZE 20
 
 int main(void){
     pid_t pid;
     int fd[2];
+    int status;
     if(pipe(fd) == -1){
         perror("Erro no Pipe");
         return 1;
@@ -19,26 +21,27 @@ int main(void){
       return -1;
     }
     if(pid == 0){
-        int readNum;
         char readStr[BUFFER_SIZE];
+        char readStr2[BUFFER_SIZE];
         close(fd[1]);
-        read(fd[0], &readNum, sizeof(int));
         read(fd[0], readStr, BUFFER_SIZE);
-        printf("Filho leu: %i & %s\n", readNum, readStr);
+        printf("%s\n", readStr);
+        read(fd[0], readStr2, BUFFER_SIZE);
+        printf("%s\n", readStr2);
         close(fd[0]);
         exit(0);
     }
-    int sendNum;
-    char sendStr[BUFFER_SIZE];
-    printf("Escreva um número: ");
-    scanf("%d", &sendNum);
-    printf("Escreva uma string: ");
-    scanf("%s", sendStr);
-
+    char sendStr[BUFFER_SIZE] = "Hello World";
+    char sendStr2[BUFFER_SIZE] = "Goodbye";
     close(fd[0]);
-    write(fd[1], &sendNum, sizeof(int));
     write(fd[1], sendStr, BUFFER_SIZE);
+    write(fd[1], sendStr2, BUFFER_SIZE);
     close(fd[1]);
+    int sonPid = wait(&status);
+    printf("Pid do filho:%d\n", sonPid);
+    if(WIFEXITED(status)){
+      printf("Exit value = %d\n", WEXITSTATUS(status));
+    }
 
     return 0;
 }
